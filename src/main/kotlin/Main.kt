24048c9b340c8177dfa2c243bcf869c1f896102b
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2024-04-06 11:34:44 -0400

    messing around

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 import com.formdev.flatlaf.FlatDarkLaf
+import java.awt.Color
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
 import javax.swing.BorderFactory
 import javax.swing.BoxLayout
+import javax.swing.GroupLayout.Alignment
 import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JPanel
@@ -15,9 +19,20 @@ fun main() {
     val panel = JPanel()
 
     panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
+
     panel.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
 
-    panel.add(JButton("Click Me!"))
+    val btn = JButton("Click me!")
+    btn.alignmentX = JButton.LEFT_ALIGNMENT
+    val box = JPanel()
+    box.background = Color.RED
+    box.alignmentX = JPanel.LEFT_ALIGNMENT
+
+    btn.addActionListener(ActionListener { 
+        e: ActionEvent? -> println("Clicked!")
+    })
+    panel.add(btn)
+    panel.add(box)
 
     window.setSize(500, 400)
     window.contentPane = panel
